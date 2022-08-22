116868586eba348034df23391d7bf3727d08a054
Lifestream308 <99936729+Lifestream308@users.noreply.github.com>
2022-08-22 14:51:11 -0700

    Add files via upload

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -1,5 +1,6 @@
 from datetime import datetime
-from flaskblog import db, login_manager
+# importing ma and bcrypt, check if this is ok
+from flaskblog import db, login_manager, ma, bcrypt
 from flask_login import UserMixin
 
 
@@ -17,6 +18,12 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
 
+# may be a problem with using __init__ because I don't want to show password here. I hash password elsewhere
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = bcrypt.generate_password_hash(password).decode('utf-8')
+
     def __repr__(self):
         return f"User('{self.username}', '{self.email}')"
         # return f"User('{self.username}', '{self.email}', '{self.image_file}')"
@@ -29,5 +36,25 @@ class Post(db.Model):
     content = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
+    def __init__(self, title, content):
+        self.title = title
+        self.content = content
+
     def __repr__(self):
         return f"Post('{self.title}', '{self.date_posted}')"
+
+
+
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ['id', 'username', 'email']
+
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
+
+class PostSchema(ma.Schema):
+    class Meta:
+        fields = ['id', 'title', 'content']
+
+post_schema = PostSchema()
+posts_schema = PostSchema(many=True)
